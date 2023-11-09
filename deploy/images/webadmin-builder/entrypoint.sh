ed7f3158859d372905e3058f0cde5637a187818c
mayorow <mayorow@gmail.com>
2023-11-09 14:55:19 +0400

    Fixed the library versions for web-admin build, avoiding new installation issues

--- deploy/images/webadmin-builder/entrypoint.sh
+++ deploy/images/webadmin-builder/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-npm init --yes
-npm install --save-dev gulp gulp-clean gulp-size gulp-concat gulp-clean-css gulp-terser
+# Should be avoided because it overwrites package.json containing fixed versions
+# npm init --yes
+npm install -f --save-dev gulp gulp-clean gulp-size gulp-concat gulp-clean-css gulp-terser
 mkdir -p dist/
 gulp
